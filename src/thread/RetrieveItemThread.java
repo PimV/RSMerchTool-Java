5ad138e93e886a8801d8cb9b6ee1a521db8a1720
Pim Verlangen <pim.verlangen@gmail.com>
2014-06-04 16:11:02 +0200

    Split up functionality in RetrieveItemThread

--- src/thread/RetrieveItemThread.java
+++ src/thread/RetrieveItemThread.java
@@ -12,9 +12,8 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.Iterator;
+import model.Category;
 import model.Item;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -39,75 +38,87 @@ public class RetrieveItemThread implements Runnable {
     @Override
     public void run() {
         try {
-
-            URL url = new URL(getAPIURL());
-
-            InputStream is = url.openStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String json = (String) readAll(br);
-
-            JSONObject rootObject = (JSONObject) new JSONParser().parse(json);
-            JSONObject itemObject = (JSONObject) new JSONParser().parse(rootObject.get("item").toString());
-            JSONObject currentObject = (JSONObject) new JSONParser().parse(itemObject.get("current").toString());
-            JSONObject todayObject = (JSONObject) new JSONParser().parse(itemObject.get("today").toString());
-            JSONObject day30Object = (JSONObject) new JSONParser().parse(itemObject.get("day30").toString());
-            JSONObject day90Object = (JSONObject) new JSONParser().parse(itemObject.get("day90").toString());
-            JSONObject day180Object = (JSONObject) new JSONParser().parse(itemObject.get("day180").toString());
-
-            //Extract itemObject
-            String name = itemObject.get("name").toString();
-            String description = itemObject.get("description").toString();
-            String membersString = itemObject.get("members").toString();
-            boolean members = false;
-            if (membersString.equals("true")) {
-                members = true;
-            }
-
-            //Extract currentObject
-            String currentTrend = currentObject.get("trend").toString();
-            String priceString = currentObject.get("price").toString();
-            int price = Integer.parseInt(priceString);
-
-            //Extract todayObject
-            String todayTrend = todayObject.get("trend").toString();
-            String todayPriceString = todayObject.get("price").toString();
-            int todayPrice = Integer.parseInt(todayPriceString);
-
-            //Extract day30Object
-            String day30Trend = day30Object.get("trend").toString();
-            String day30Change = day30Object.get("change").toString();
-
-            //Extract day90Object
-            String day90Trend = day90Object.get("trend").toString();
-            String day90Change = day90Object.get("change").toString();
-
-            //Extract day180Object
-            String day180Trend = day180Object.get("trend").toString();
-            String day180Change = day180Object.get("change").toString();
-
-            //Create a new Item
             Item i = new Item();
-
-            //Set values for this new Item
-            i.setName(name);
-            i.setDescription(description);
-            i.setPrice(price);
-            i.setCurrentTrend(currentTrend);
-            i.setTodayPrice(todayPrice);
-            i.setTodayTrend(todayTrend);
-            i.setDay30Trend(day30Trend);
-            i.setDay30Change(day30Change);
-            i.setDay90Trend(day90Trend);
-            i.setDay90Change(day90Change);
-            i.setDay180Trend(day180Trend);
-            i.setDay180Change(day180Change);
-            i.setMembers(members);
+            retrieveItemInformation(i);
+            retrieveAccuratePriceInformation(i);
 
         } catch (Exception e) {
             System.err.println("Error retrieving item with itemId: " + this.itemId);
             e.printStackTrace();
         }
+    }
+
+    private Item retrieveItemInformation(Item i) throws Exception {
+        URL itemInformation = new URL(getItemInformationURL());
+        InputStream is = itemInformation.openStream();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+        String json = (String) readAll(br);
+
+        JSONObject rootObject = (JSONObject) new JSONParser().parse(json);
+        JSONObject itemObject = (JSONObject) new JSONParser().parse(rootObject.get("item").toString());
+        JSONObject currentObject = (JSONObject) new JSONParser().parse(itemObject.get("current").toString());
+        JSONObject todayObject = (JSONObject) new JSONParser().parse(itemObject.get("today").toString());
+        JSONObject day30Object = (JSONObject) new JSONParser().parse(itemObject.get("day30").toString());
+        JSONObject day90Object = (JSONObject) new JSONParser().parse(itemObject.get("day90").toString());
+        JSONObject day180Object = (JSONObject) new JSONParser().parse(itemObject.get("day180").toString());
+
+        //Extract itemObject
+        String name = itemObject.get("name").toString();
+        String description = itemObject.get("description").toString();
+        String membersString = itemObject.get("members").toString();
+        boolean members = false;
+        if (membersString.equals("true")) {
+            members = true;
+        }
+        String categoryString = itemObject.get("type").toString();
+        categoryString = categoryString.replace(' ', '_');
+        categoryString = categoryString.toUpperCase();
+        Category category = Category.get(categoryString);
+
+        //Extract currentObject
+        String currentTrend = currentObject.get("trend").toString();
+        String priceString = currentObject.get("price").toString();
+        int price = Integer.parseInt(priceString);
+
+        //Extract todayObject
+        String todayTrend = todayObject.get("trend").toString();
+        String todayPriceString = todayObject.get("price").toString();
+        int todayPriceChange = Integer.parseInt(todayPriceString);
+
+        //Extract day30Object
+        String day30Trend = day30Object.get("trend").toString();
+        String day30Change = day30Object.get("change").toString();
+
+        //Extract day90Object
+        String day90Trend = day90Object.get("trend").toString();
+        String day90Change = day90Object.get("change").toString();
+
+        //Extract day180Object
+        String day180Trend = day180Object.get("trend").toString();
+        String day180Change = day180Object.get("change").toString();
+
+        //Set values for this new Item
+        i.setName(name);
+        i.setDescription(description);
+        i.setPrice(price);
+        i.setCurrentTrend(currentTrend);
+        i.setTodayPriceChange(todayPriceChange);
+        i.setTodayTrend(todayTrend);
+        i.setDay30Trend(day30Trend);
+        i.setDay30Change(day30Change);
+        i.setDay90Trend(day90Trend);
+        i.setDay90Change(day90Change);
+        i.setDay180Trend(day180Trend);
+        i.setDay180Change(day180Change);
+        i.setMembers(members);
+        i.setCategory(category);
+        System.out.println(i);
+        return i;
+    }
+    
+    private Item retrieveAccuratePriceInformation(Item i) {
+        return i;
     }
 
     private String readAll(Reader reader) throws IOException {
@@ -121,8 +132,12 @@ public class RetrieveItemThread implements Runnable {
         return sb.toString();
     }
 
-    private String getAPIURL() {
+    private String getItemInformationURL() {
         return "http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=" + this.itemId;
+    }
+
+    private String getAccurateItemPriceURL() {
+        return "http://services.runescape.com/m=itemdb_rs/api/graph/" + itemId + ".json";
     }
 
 }
